a133d682fd22562e91a0d3ed1e07d40a01c99f26
aleksei.makarov <alemakar@t-systems.ru>
2020-03-25 13:17:28 +0300

    small change for getting languages

--- src/main/java/phonosemantics/language/languageReduced/LanguageService.java
+++ src/main/java/phonosemantics/language/languageReduced/LanguageService.java
@@ -25,66 +25,70 @@ public class LanguageService {
      * READING ALL THE LANGUAGES WITH AVAILABLE PHONOLOGY
      */
     public static void readAllLanguagesFromFile() {
-        allLanguages = new HashMap<>();
-        userLogger.info("reading languages from file");
+        if (allLanguages == null) {
+            allLanguages = new HashMap<>();
+            userLogger.info("reading languages from file");
 
-        try {
-            InputStream inputStream = new FileInputStream(INPUT_LANGUAGES_PATH);
-            Workbook wb = WorkbookFactory.create(inputStream);
-            inputStream.close();
-            Sheet sheet = wb.getSheetAt(0);
+            try {
+                InputStream inputStream = new FileInputStream(INPUT_LANGUAGES_PATH);
+                Workbook wb = WorkbookFactory.create(inputStream);
+                inputStream.close();
+                Sheet sheet = wb.getSheetAt(0);
 
-            int rowNum = 1;
-            Row row = sheet.getRow(rowNum);
-            Cell cell = row.getCell(0);
-            String[] allPhArr = null;
+                int rowNum = 1;
+                Row row = sheet.getRow(rowNum);
+                Cell cell = row.getCell(0);
+                String[] allPhArr = null;
 
-            // LOOKING FOR LANGUAGE
-            while (cell.getCellType() != CellType.BLANK) {
+                // LOOKING FOR LANGUAGE
+                while (cell.getCellType() != CellType.BLANK) {
 
-                String languageName = cell.getStringCellValue();
-                if (languageName == null) {
-                    break;
-                }
+                    String languageName = cell.getStringCellValue();
+                    if (languageName == null) {
+                        break;
+                    }
 
-                Set<PhonemeInTable> languagePhonology = new HashSet<>();
-                for (PhonemeInTable ph : PhonemesBank.getInstance().getAllPhonemesList()) {
-                    if (allPhArr == null) {
-                        String allPh = " ";
-                        Row r = sheet.getRow(rowNum);
+                    Set<PhonemeInTable> languagePhonology = new HashSet<>();
+                    for (PhonemeInTable ph : PhonemesBank.getInstance().getAllPhonemesList()) {
+                        if (allPhArr == null) {
+                            String allPh = " ";
+                            Row r = sheet.getRow(rowNum);
 
-                        for (int i = 1; i <= NUM_OF_PHONOLOGY_COLUMNS; i++) {
-                            if (r.getCell(i) != null) {
-                                allPh += r.getCell(i).getStringCellValue() + " ";
+                            for (int i = 1; i <= NUM_OF_PHONOLOGY_COLUMNS; i++) {
+                                if (r.getCell(i) != null) {
+                                    allPh += r.getCell(i).getStringCellValue() + " ";
+                                }
+                            }
+                            allPhArr = allPh.split(" ");
+                        }
+
+                        for (String symbol : allPhArr) {
+                            if (symbol.equals(ph.getValue())) {
+                                userLogger.debug(ph.getValue() + " "); //check all phonemes in console
+                                languagePhonology.add(ph);
                             }
                         }
-                        allPhArr = allPh.split(" ");
                     }
 
-                    for (String symbol: allPhArr) {
-                        if (symbol.equals(ph.getValue())) {
-                            userLogger.debug(ph.getValue() + " "); //check all phonemes in console
-                            languagePhonology.add(ph);
-                        }
-                    }
+                    Language language = new Language(languageName, languagePhonology);
+                    userLogger.info("new language: " + language.getTitle());
+                    allLanguages.put(languageName, language);
+
+                    rowNum++;
+                    cell = sheet.getRow(rowNum).getCell(0);
                 }
 
-                Language language = new Language(languageName, languagePhonology);
-                userLogger.info("new language: " + language.getTitle());
-                allLanguages.put(languageName, language);
-
-                rowNum++;
-                cell = sheet.getRow(rowNum).getCell(0);
+            } catch (IOException e) {
+                userLogger.error(e.toString());
             }
-
-        } catch (IOException e) {
-            userLogger.error(e.toString());
         }
     }
 
 
     public static void readAllLanguageNamesFromFile() {
-        allLanguages = new HashMap<>();
+        if (allLanguages == null) {
+            allLanguages = new HashMap<>();
+        }
 
         try {
             InputStream inputStream = new FileInputStream(INPUT_LANGUAGES_PATH);
@@ -155,9 +159,7 @@ public class LanguageService {
      * GET ALL LANGUAGES
      */
     public static HashMap<String, Language> getAllLanguages() {
-        if (allLanguages == null) {
-            readAllLanguagesFromFile();
-        }
+        readAllLanguagesFromFile();
         for (Map.Entry<String, Language> languageEntry : allLanguages.entrySet()) {
             languageEntry.getValue().readLangPhonologyFromFile();
         }
