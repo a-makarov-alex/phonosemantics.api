e93f89fc68e940d888e3e83c9c855d7aef3d968f
aleksei.makarov <alemakar@t-systems.ru>
2020-04-09 00:37:18 +0300

    meaning class draft

--- src/main/java/phonosemantics/meaning/Meaning.java
+++ src/main/java/phonosemantics/meaning/Meaning.java
@@ -1,23 +1,33 @@
 package phonosemantics.meaning;
 
+import java.util.HashMap;
+
 public class Meaning {
     private String definition;
-    //private boolean tag_big = false;
-    //private boolean tag_small = false;
+    private HashMap<SemanticTag, Boolean> tags;
 
     public Meaning(String definition) {
         this.definition = definition;
+        // TODO: bank of tags for definitions
+        // записывать теги тут, получать список из отдельного метода, где захардкожены эти теги.
     }
 
     public String getDefinition() {
         return definition;
     }
 
-//    public boolean isTag_big() {
-//        return tag_big;
-//    }
-//
-//    public boolean isTag_small() {
-//        return tag_small;
-//    }
+    public HashMap<SemanticTag, Boolean> getTags() {
+        return tags;
+    }
+
+    public boolean checkTag(String tagName) {
+        SemanticTag sTag = SemanticTag.valueOf(tagName.toUpperCase());
+        return this.tags.get(sTag);
+    }
+
+    public enum SemanticTag {
+        BIG, SMALL,
+        ROUND, STRAIGHT,        // реально надо прямой/кривой и круглый/неокруглый
+        DULL, SHARP
+    }
 }
--- src/main/java/phonosemantics/meaning/MeaningController.java
+++ src/main/java/phonosemantics/meaning/MeaningController.java
@@ -1,4 +1,9 @@
 package phonosemantics.meaning;
 
 public class MeaningController {
+
+    // TODO:
+    // 1. get all meanings (check WL controller)
+    // 2. get "opposition tags" by meaning
+
 }
